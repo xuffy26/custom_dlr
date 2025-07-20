da1cdcea84fbe516e0b82968983d5797f4f20c3a
xuffy26 <nisharjs123@gmail.com>
2025-07-21 00:15:04 +0530

    Update apps.py

--- custom_dlr/apps.py
+++ custom_dlr/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
-from django.conf import settings
 
-class MyAppConfig(AppConfig):
-    name = 'myapp'
-
-    def ready(self):
-        from apscheduler.schedulers.background import BackgroundScheduler
-        if not settings.SCHEDULER.running:
-            settings.SCHEDULER.start()
+class CustomDlrConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'custom_dlr'
