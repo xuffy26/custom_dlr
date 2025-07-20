a85c56592b533499aadb3222c713574d04bcff34
Nizarudeen M <nisharjs123@gmail.com>
2025-07-20 22:38:00 +0530

    Add init and manage.py

---
+++ __init__.py
@@ -0,0 +1,1 @@
+# custom_dlr/__init__.py
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+# custom_dlr/wsgi.py
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "custom_dlr.settings")
+application = get_wsgi_application()
