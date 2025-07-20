cbd27f66af0288a9519fad9511d4b60c3c0a4210
xuffy26 <nisharjs123@gmail.com>
2025-07-20 23:07:55 +0530

    Update settings.py

--- custom_dlr/settings.py
+++ custom_dlr/settings.py
@@ -1,4 +1,26 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'your-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = ['*']  # Update for production
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+    'django.contrib.auth',
+    'django.contrib.admin',
+]
+
+MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+]
+
+ROOT_URLCONF = 'custom_dlr.urls'
+
+WSGI_APPLICATION = 'custom_dlr.wsgi.application'
 
 SCHEDULER = BackgroundScheduler()
 SCHEDULER.start()
