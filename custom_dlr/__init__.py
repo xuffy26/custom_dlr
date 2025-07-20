d6e0c8690e7d61c1116d20e64fa61b1ca081b679
xuffy26 <nisharjs123@gmail.com>
2025-07-21 00:14:03 +0530

    Update __init__.py

--- custom_dlr/__init__.py
+++ custom_dlr/__init__.py
@@ -1,1 +1,7 @@
-# custom_dlr/__init__.py
+from __future__ import absolute_import, unicode_literals
+
+# This will make sure the app is always imported when
+# Django starts so that shared tasks are registered
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
